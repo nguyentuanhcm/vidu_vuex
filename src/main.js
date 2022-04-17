d9dda50f931f132f791c4688e542e4371d56074a
nguyentuanhcm <nguyentuan88.hcm@gmail.com>
2022-04-17 23:48:00 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Vuex from 'vuex'
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state,x) {
+      state.count = state.count+x;
+    }
+  }, 
+  getters: {
+    getCount(state) {
+      return state.count;
+    }
+  },
+  actions: {
+    setCount(context, x) {
+      x = x+2;
+      context.commit('increment',x);
+
+    }
+  }
+})
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
